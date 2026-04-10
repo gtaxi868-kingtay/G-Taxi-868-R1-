e50bf6779ef81a00f08e802184e457ea47442963
Gershwane Taylor <gtaxi868@gmail.com>
2026-04-10 14:20:58 -0400

    fix: align rider to RN 0.81.5 Expo SDK 54 stable

--- apps/rider/babel.config.js
+++ apps/rider/babel.config.js
@@ -8,6 +8,7 @@ module.exports = function (api) {
                     'shared': '../../shared',
                 },
             }],
+            'react-native-worklets/plugin',
             'react-native-reanimated/plugin',
         ],
     };
